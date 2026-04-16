67599709da53175087ca96a0e9087b6fd791100e
Umer Zafar <umerzafar4598@gmail.com>
2026-04-16 17:46:51 +0500

    Experience Section added + responsivness modifications:

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,17 @@ import 'animate.css';
 import AosWrapper from "@/components/helper/AosWrapper";
 import SkillsSection from "@/components/sections/Skills/Skills";
 import Projects from "@/components/sections/Projects/Projects";
+import Experience from "@/components/sections/Experience/Experience";
 
 
 export default function Home() {
   return (
-    <main className="relative bg-foreground text-background">
+    <main className="relative bg-foreground text-background w-full flex flex-col items-center">
       <AosWrapper>
         <FloatingNav navItems={navItems} />
         <Hero />
         <About />
+        <Experience />
         <SkillsSection />
         <Projects />
       </AosWrapper>
--- components/helper/AosWrapper.tsx
+++ components/helper/AosWrapper.tsx
@@ -5,11 +5,15 @@ import AOS from "aos"
 
 const AosWrapper = ({ children }: { children: React.ReactNode }) => {
     useEffect(() => {
+        window.history.scrollRestoration = 'manual'
+        window.scrollTo(0, 0)
+
         AOS.init({
             duration: 650,
             easing: "ease-in-out",
             once: true,
             mirror: false,
+            offset: 0,
         })
     }, [])
 
